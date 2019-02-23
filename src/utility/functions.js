78073a24f81e3550d72ecf10c5517c66855b7315
unknown <cmkoivu@comcast.net>
2019-02-23 08:13:43 -0500

    commented functions file

--- src/utility/functions.js
+++ src/utility/functions.js
@@ -1,5 +1,18 @@
-/** updated passed state object array with passed updated object,
- *  state object array has a unique id key for each object
+
+/**
+ * This file is a library of functions used in this project.
+ * I am wanting to keep the components as clean as possible 
+ * by putting this logic in here
+ *
+ */
+
+
+
+/**
+ * passing in the state object and creating a new immutable state
+ * array with the payload object update
+ * @param {*} stateObjArr 
+ * @param {*} payloadObj 
  */
   export function updateObjectInArray(stateObjArr, payloadObj) {  
     return stateObjArr.map( (item => {
@@ -16,6 +29,13 @@
     }));
   }
 
+  /**
+   * Since i am pulling data from two data sources, I am doing
+   * a join to get an author name from the user array, and 
+   * matching it to a user id in the post array
+   * @param {*} userArray 
+   * @param {*} userId 
+   */
 
   export function getAuthorName(userArray, userId){      
        // search the users array and return author name   
@@ -29,7 +49,10 @@
         }));   
   }
 
-       // verify if object is empty 
+  /**
+   * verify if a passed in object is empty
+   * @param {*} obj 
+   */
   export function isObjectEmpty(obj) {
     for(var key in obj) {
       if(obj.hasOwnProperty(key))
